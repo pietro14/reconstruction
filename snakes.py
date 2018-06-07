c6ab285dccd2a05fa29df25ea85334c07a460a7b
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-06-07 11:58:11 +0200

    first version with major and minor axes

--- snakes.py
+++ snakes.py
@@ -15,25 +15,21 @@ from morphsnakes import(morphological_chan_vese,
 from skimage import measure
 import matplotlib.pyplot as plt
 
-class Pixel:
-    def __init__(self,x,y,z):
-        self.x=x
-        self.y=y
-        self.z=z
-
-class SnakeSpec:
+class Cluster:
     def __init__(self,hits):
-        self.size = len(hits)
-        self.hist = hits
+        self.hits = hits
     def integral(self):
-        pass
-        
+        return sum([z for (x,y,z) in self.hits])
+    def size(self):
+        return len(self.hits)
+    def dump(self):
+        print self.hits
+    
 class SnakesFactory:
     def __init__(self,th2,name):
         self.data = self.getData(th2)
         self.X = self.getActiveCoords(th2)
         self.contours = []
-        self.filledContours = []
         self.name = name
         
     def store_evolution_in(self,lst):
@@ -69,7 +65,7 @@ class SnakesFactory:
             for x_bin in xrange(x_bins): 
                 z = th2.GetBinContent(x_bin + 1,y_bin + 1)
                 if z>0:
-                    ret.append((x_bin,y_bin))
+                    ret.append((x_bin,y_bin,z))
         return np.array(ret)
     
     def getContours(self,iterations,threshold=0.69):
@@ -92,27 +88,34 @@ class SnakesFactory:
         self.contours = ls
         return ls
 
-    def getClusters(self,maxDist=20,minPoints=4):
+    def plot_axis(self,center, dir, num_steps=100, step_size=0.5):
+        line_x = []
+        line_y = []
+        for i in range(num_steps):
+            dist_from_center = step_size * (i - num_steps / 2)
+            point_on_line = center + dist_from_center * dir
+            line_x.append(point_on_line[0])
+            line_y.append(point_on_line[1])
+        return (line_x, line_y)
+
+    def getClusters(self,maxDist=100,minPoints=10,minPointsCore=3,plot=True):
+
         from sklearn.cluster import DBSCAN
         from sklearn import metrics
-        from sklearn.datasets.samples_generator import make_blobs
-        from sklearn.preprocessing import StandardScaler
         from scipy.spatial import distance
-        # make the clustering with DBSCAN algo
 
+        # make the clustering with DBSCAN algo
         X = self.X
         distance_matrix = distance.squareform(distance.pdist(X))
-        db = DBSCAN(eps=maxDist, min_samples=minPoints).fit(distance_matrix)
+        db = DBSCAN(eps=maxDist, min_samples=minPoints,n_jobs=-1).fit(distance_matrix)
         core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
         core_samples_mask[db.core_sample_indices_] = True
         labels = db.labels_
-
+        
         # Number of clusters in labels, ignoring noise if present.
         n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
-        print 'Estimated number of clusters: %d' % n_clusters_
-        print "Silhouette Coefficient: %0.3f" % metrics.silhouette_score(X, labels)
-
+        clusters = []
         ##################### plot
         import matplotlib.pyplot as plt
 
@@ -128,17 +131,47 @@ class SnakesFactory:
             class_member_mask = (labels == k)
          
             xy = X[class_member_mask & core_samples_mask]
-            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                     markeredgecolor='k', markersize=14)
-         
-            xy = X[class_member_mask & ~core_samples_mask]
-            plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                     markeredgecolor='k', markersize=6)
-            
-        plt.title('Estimated number of clusters: %d' % n_clusters_)
-        plt.show()
+            x = xy[:, 0]; y = xy[:, 1]
+            if plot: plt.plot(x, y, 'o', markerfacecolor=tuple(col),
+                              markeredgecolor='k', markersize=14)
 
-    def plotContours(self,contours,fill=False):
+            # only add the cores to the clusters saved in the event
+            if len(xy)>minPointsCore:
+                cl = Cluster(xy)
+                clusters.append(cl)
+
+                print "cluster size = ",len(xy)
+                covmat = np.cov([x,y])
+                print covmat
+
+                eig_values, eig_vecs = np.linalg.eig(covmat)
+                indexes = (np.argmax(eig_values),np.argmin(eig_values))
+                eig_vec_vals = (eig_vecs[:, indexes[0]], eig_vecs[:, indexes[-1]])
+                print "largest,smallest eignevalues = ",eig_vec_vals
+
+                if plot:
+                    mean_point=np.array([np.mean(x),np.mean(y)])
+                    lines = [self.plot_axis(mean_point, ev) for ev in eig_vec_vals] 
+                    for line in lines:
+                        plt.plot(line[0], line[1], c="r")
+
+                
+            # plot also the non-core hits
+            xy = X[class_member_mask & ~core_samples_mask]
+            if plot: plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                              markeredgecolor='k', markersize=6)
+
+        if plot:
+            plt.title('Estimated number of clusters: %d' % n_clusters_)
+            #plt.show()
+            for ext in ['png','pdf']:
+                plt.savefig('{name}.{ext}'.format(name=self.name,ext=ext))
+            plt.gcf().clear()
+
+            
+        return clusters
+        
+    def plotContours(self,contours):
 
         image = img_as_float(self.data)
         #fig, axes = plt.subplots(1, 2, figsize=(8, 4))
@@ -169,20 +202,3 @@ class SnakesFactory:
         for ext in ['png','pdf']:
             plt.savefig('{name}.{ext}'.format(name=self.name,ext=ext))
             
-    def filledSnakes(self,snakes,snakeMinSize=4,hitThr=0):
-        if len(self.contours)==0: self.getContours(iterations)
-
-        # fill the snakes so that the inside of the snake is all tagged as signal
-        from scipy import ndimage as ndi
-        tmpfilled = ndi.binary_fill_holes(self.contours)
-
-        # cleanup spurious small snakes
-        from skimage import morphology
-        self.filledContours = morphology.remove_small_objects(tmpfilled, snakeMinSize)
-        print "Len of filled contours = ",len(self.filledContours)
-        
-        print "Found ",len(self.contours)
-        # for cnt in self.contours:
-        #     if sum(cnt)>0:
-        #         print "contour = ",cnt
-        #         print "len = ",len(cnt)
