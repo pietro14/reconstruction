66637f5710802104f66c7250372d78e087a60065
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2020-10-16 16:09:01 +0200

    after the central installation of python

--- scripts/activate_cygno_lngs.sh
+++ scripts/activate_cygno_lngs.sh
@@ -1,8 +1,7 @@
 echo "Entering the CYGNO virtual environment software..."
-export PATH="/nfs/cygno/software/python3.8/pyenv/bin:$PATH"
 export LD_LYBRARY_PATH="/nfs/cygno/software/python3.8/pyenv/lib"
 echo "Setting up ROOT..."
-export PYTHONPATH="/nfs/cygno/software/python3.8/env/lib/python3.8/site-packages:/nfs/cygno/software/python3.8/env/lib/python3.8/site-packages-pipinstalled"
+export PYTHONPATH=$PYTHONPATH:/home/$USER/.local/lib/python3.8/site-packages/
 source /nfs/cygno/software/root-v6-22-00-py36-build/bin/thisroot.sh 
 alias python="python3.8"
 echo "DONE."
