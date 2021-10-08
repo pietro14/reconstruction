452526b03c84a4eeac6e7a9756e27a6072abdca8
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2021-10-08 16:25:25 +0200

    general update to GBR likelihood training

--- corrections/gbr_trainer.py
+++ corrections/gbr_trainer.py
@@ -4,15 +4,36 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 
 import numpy as np
-from root_numpy import tree2array
+from root_numpy import tree2array,fill_hist
 
 import matplotlib.pyplot as plt
 import numpy as np
+import joblib
 
 from sklearn import ensemble
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+
+def getCanvas(name='c'):
+
+    ROOT.gStyle.SetPalette(ROOT.kRainBow)
+    ROOT.gStyle.SetNumberContours(51)
+    ROOT.gErrorIgnoreLevel = 100
+
+    c = ROOT.TCanvas(name,'',1200,1200)
+    lMargin = 0.14
+    rMargin = 0.10
+    bMargin = 0.15
+    tMargin = 0.10
+    c.SetLeftMargin(lMargin)
+    c.SetRightMargin(rMargin)
+    c.SetTopMargin(tMargin)
+    c.SetBottomMargin(bMargin)
+    c.SetFrameBorderMode(0);
+    c.SetBorderMode(0);
+    c.SetBorderSize(0);
+    return c
 
 
 class GBRLikelihoodTrainer:
@@ -21,6 +42,9 @@ class GBRLikelihoodTrainer:
         self.target = params['target']
         self.var = params['inputs']
         self.cuts_base = params['selection']
+        self.X_test = None
+        self.y_test = None
+        self.training = False
         
     def __init__(self,params):
         self.set_defaults(params)
@@ -51,26 +75,39 @@ class GBRLikelihoodTrainer:
         return X,y
 
     def train_model(self,X,y):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=13)
-        reg = ensemble.GradientBoostingRegressor(**self.training_params)
-        reg.fit(X_train, y_train)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=13)
 
-        mse = mean_squared_error(y_test, reg.predict(X_test))
+        self.models_ = {}
+
+        # MEAN SQUARE ERRORS REGRESSION
+        print("===> Training mean square errors regression...")
+        reg_ls = ensemble.GradientBoostingRegressor(loss='ls',
+                                                    **self.training_params)
+        self.models_["mse"] = reg_ls.fit(X_train, y_train)
+        mse = mean_squared_error(y_test, reg_ls.predict(X_test))
         print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
 
+        # QUANTILE REGRESSION
+        print("===> Now training quantiles regression...")
+        for alpha in [0.5]: #[0.05, 0.5, 0.95]:
+            print("\t### Quantile = ",alpha)
+            reg = ensemble.GradientBoostingRegressor(loss='quantile', alpha=alpha,
+                                                     **self.training_params)
+            self.models_["q%1.2f" % alpha] = reg.fit(X_train, y_train)
+        
         self.X_test = X_test
         self.y_test = y_test
-        self.reg_ = reg
+        self.training = True
 
     def plot_training(self):
         test_score = np.zeros((self.training_params['n_estimators'],), dtype=np.float64)
-        for i, y_pred in enumerate(self.reg_.staged_predict(self.X_test)):
-            test_score[i] = self.reg_.loss_(self.y_test, y_pred)
+        for i, y_pred in enumerate(self.models_["mse"].staged_predict(self.X_test)):
+            test_score[i] = self.models_["mse"].loss_(self.y_test, y_pred)
 
         fig = plt.figure(figsize=(6, 6))
         plt.subplot(1, 1, 1)
         plt.title('Deviance')
-        plt.plot(np.arange(self.training_params['n_estimators']) + 1, self.reg_.train_score_, 'b-',
+        plt.plot(np.arange(self.training_params['n_estimators']) + 1, self.models_["mse"].train_score_, 'b-',
                  label='Training Set Deviance')
         plt.plot(np.arange(self.training_params['n_estimators']) + 1, test_score, 'r-',
                  label='Test Set Deviance')
@@ -80,7 +117,7 @@ class GBRLikelihoodTrainer:
         plt.savefig('training.png',bbox_inches='tight', pad_inches=0)
       
         # plot variables importance
-        feature_importance = self.reg_.feature_importances_
+        feature_importance = self.models_["mse"].feature_importances_
         sorted_idx = np.argsort(feature_importance)
         pos = np.arange(sorted_idx.shape[0]) + .5
         fig = plt.figure(figsize=(12, 6))
@@ -89,7 +126,7 @@ class GBRLikelihoodTrainer:
         plt.yticks(pos, np.array(self.variables())[sorted_idx])
         plt.title('Feature Importance (MDI)')
         
-        result = permutation_importance(self.reg_, self.X_test, self.y_test, n_repeats=10,
+        result = permutation_importance(self.models_["mse"], self.X_test, self.y_test, n_repeats=10,
                                         random_state=42, n_jobs=2)
         sorted_idx = result.importances_mean.argsort()
         plt.subplot(1, 2, 2)
@@ -99,16 +136,65 @@ class GBRLikelihoodTrainer:
         plt.savefig('variables_importance.png')
 
         
-    def save_model(self,filename):
-        import joblib
-        joblib.dump(self.reg_,filename)
-    
+    def save_models(self,prefix):
+        for k in self.models_:
+            filename = "{prefix}_{k}.sav".format(prefix=prefix.split('.')[0].replace(" ",""),k=k)
+            print ("Saving model {k} in file {f}".format(k=k,f=filename))
+            joblib.dump(self.models_[k],filename)
+        print("Models saved.")
+
+    def get_testdata(self):
+        return self.X_test, self.y_test
+
+    def test_models(self,recofile,prefix):
+        print ("Test the saved models on the input file: ",recofile)
+        # use the ones of the object if testing after training (to ensure orthogonality wrt training sample)
+        if self.training:
+            X_test = self.X_test
+            y_test = self.y_test
+        else:
+            X,y = self.get_dataset(recofile)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=13)
+
+        hist = ROOT.TH1F('hist','',30,0.1,1.5)
+            
+        c = getCanvas('c')
+        hists = {}
+        maxy=-1
+        for k in ['mse','q0.05','q0.50','q0.95']:
+            filename = "{prefix}_{k}.sav".format(prefix=prefix.split('.')[0].replace(" ",""),k=k)
+            print("Sanity check of the saved GBR model in the output file ",filename)
+            model = joblib.load(filename)
+            result = model.score(X_test, y_test)
+            print("The precision on the test data is: ",result)
+            y_pred = model.predict(X_test)
+            hists[k] = hist.Clone('hist_{k}'.format(k=k))
+            #print ("Regressed values = ",y_pred)
+            fill_hist(hists[k],y_pred)
+            maxy = max(maxy,hists[k].GetMaximum())
+            
+        hists["uncorr"] = hist.Clone('hist_uncorr')
+        fill_hist(hists["uncorr"],y_test)
+        colors = {'uncorr': ROOT.kRed, 'mse': ROOT.kCyan, 'q0.50': ROOT.kBlack}
+        styles = {'uncorr': 3005, 'mse': 3004, 'q0.50': 0}
+        for i,k in enumerate(colors):
+            drawopt = '' if i==0 else 'same'
+            print ("Filling hist ",k," with color ",colors[k])
+            hists[k].SetLineColor(colors[k])
+            hists[k].SetFillColor(colors[k])
+            hists[k].SetFillStyles(styles[k])
+            hists[k].SetLineWidth(2)
+            hists[k].SetMaximum(1.2 * maxy)
+            hists[k].Draw("hist {opt}".format(opt=drawopt))
+        c.SaveAs("energy.png")
+        
         
 if __name__ == '__main__':
     from optparse import OptionParser
 
     parser = OptionParser(usage='%prog input.root params_gbrtrain.txt [opts] ')
-    parser.add_option('-o', '--outname', dest='outname', default='gbrLikelihood.sav', type='string', help='output name of the regression model')
+    parser.add_option('-o', '--outname', dest='outname', default='gbrLikelihood', type='string', help='prefix for the output name of the regression models')
+    parser.add_option('-a', '--apply-only', dest='applyOnly', action='store_true', default=False,  help='only apply regression on saved models and the data of the input file. Do not train')
     (options, args) = parser.parse_args()
 
     recofile = args[0]
@@ -116,9 +202,19 @@ if __name__ == '__main__':
     params = eval(config.read())
 
     GBR = GBRLikelihoodTrainer(params)
-    X,y = GBR.get_dataset(recofile)
+    if options.applyOnly == False:
+        X,y = GBR.get_dataset(recofile)
+        print("Dataset loaded from file ",args[0], " Now train the model.")
     
-    GBR.train_model(X,y)
-    GBR.plot_training()
-    GBR.save_model(options.outname)
+        GBR.train_model(X,y)
+        print("GBR likelihood computed. Now plot results and control plots")
+    
+        GBR.plot_training()
+        GBR.save_models(options.outname)
 
+    # now test the model (this is to test that the model was saved correctly)
+    GBR.test_models(recofile,options.outname)
+    
+    print("DONE.")
+    
+    
