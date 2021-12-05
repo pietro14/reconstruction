0e966bc26c5deb26141aed520234b23da8720309
igorabritta <igorabritta@gmail.com>
2021-12-05 18:06:13 +0100

    correction of the max-entries loop for the pedestal calculation

--- reconstruction.py
+++ reconstruction.py
@@ -153,7 +153,7 @@ class analysis:
                 run = int(m.group(1))
                 event = int(m.group(2))
             if event in self.options.excImages: continue
-            if maxImages>-1 and i>min(len(tf.GetListOfKeys()),maxImages): break
+            if maxImages>-1 and event>min(len(tf.GetListOfKeys()),maxImages): break
                 
             if not obj.InheritsFrom('TH2'): continue
             print("Calc pedestal mean with event: ",name)
@@ -177,7 +177,7 @@ class analysis:
                 run = int(m.group(1))
                 event = int(m.group(2))
             if event in self.options.excImages: continue
-            if maxImages>-1 and i>min(len(tf.GetListOfKeys()),maxImages): break
+            if maxImages>-1 and event>min(len(tf.GetListOfKeys()),maxImages): break
 
             if not obj.InheritsFrom('TH2'): continue
             print("Calc pedestal rms with event: ",name)
