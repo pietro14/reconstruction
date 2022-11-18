5e44cc90f2e6300404a3e0ea40107db4def6122b
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-11-18 18:04:08 +0100

    changes in the autumn22 noise threshold parameter, substitute the bash parsing script with python one, add more runs to filtered runlog

---
+++ scripts/parse_runlog.py
@@ -0,0 +1,30 @@
+import os,sys,optparse,csv
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser(usage='usage: %prog runlog.csv [opts] ', version='%prog 1.0')
+    parser.add_option('-m', '--runMin'   , type='int'       , default=0 , help='minimum run number')
+    parser.add_option('-M', '--runMax'   , type='int'       , default=99999 , help='maximum run number')
+    parser.add_option('-o', '--output'   , type='string'    , default="filtered_runlog.csv" , help='output cleaned run log')
+    parser.add_option('-H', '--includeHeader'   , action = 'store_true', default=False, help='include the header row (typically when remaking the full CSV)')
+
+    (options, args) = parser.parse_args()
+
+    inputRunlog = args[0]
+    outputRunlog = open(options.output,'w')
+     
+    with open(inputRunlog,"r") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        # This skips the first row (header) of the CSV file.
+        if not options.includeHeader:
+            next(csvreader)
+        for row in list(csvreader):
+            cleanrow = [f.replace('"','') for f in row]
+            if options.includeHeader and row[0]=="run_number":
+                outputRunlog.write(' , '.join(row[:-3])+'\n')
+            else:
+                run=int(row[0])
+                if run<options.runMin or run>options.runMax or 'NULL' in row[-1]: continue
+                outputRunlog.write(' , '.join(row[:-3])+'\n')
+
+    outputRunlog.close()
+    print("Filtered runlog in ",options.output)
