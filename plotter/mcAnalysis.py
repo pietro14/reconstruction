f4cb4bc1d04d5fcb28658cb22a6c0d2b241c6e02
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-12-05 18:22:02 +0100

    updates to plotter for LNF analysis

--- plotter/mcAnalysis.py
+++ plotter/mcAnalysis.py
@@ -212,14 +212,13 @@ class MCAnalysis:
                     if not os.path.exists(countersfile):
                         print ("Warning: counters.txt file not present. Assuming no skim and taking the process normalization from file for component %s" % cnames)
                         ## get the counts from the histograms instead of pickle file (smart, but extra load for ROOT from EOS it seems)
-                        total_w += sum(tty.getEntries() for tty in self._allData[pname])
+                        ROOT.gEnv.SetValue("TFile.AsyncReading", 1);
+                        tmp_rootfile = ROOT.TFile.Open(rootfile+"?readaheadsz=65535")
+                        tmp_tree     = tmp_rootfile.Get('Events')
                         is_w = 0
-<<<<<<< HEAD
                         total_w += tmp_tree.GetEntries()
-                        print ("total w = ",total_w)
-=======
->>>>>>> origin/summer22
                         scale = "(%s)" % field[2]
+                        tmp_rootfile.Close()
                     else:
                         countersobj  = open(countersfile, "r")
                         counters = eval(countersobj.read())
@@ -245,10 +244,7 @@ class MCAnalysis:
                 else:
                     print("Poorly formatted line: ", field)
                     raise RuntimeError
-<<<<<<< HEAD
-=======
-                
->>>>>>> origin/summer22
+
                 # Adjust free-float and fixed from command line
                 for p0 in options.processesToFloat:
                     for p in p0.split(","):
@@ -278,13 +274,8 @@ class MCAnalysis:
                     myvariations[0].name = "norm_"+tty.getOption('PegNormToProcess')
                     print("Overwrite the norm systematic for %s to make it correlated with %s" % (pname, tty.getOption('PegNormToProcess')))
                 if pname not in self._rank: self._rank[pname] = len(self._rank)
-<<<<<<< HEAD
             if to_norm: 
                 for tty in ttys: tty.setScaleFactor("%s*%g" % (scale, 1.0/total_w))
-=======
-            if to_norm:
-                for tty in ttys: tty.setScaleFactor("%s" % scale)
->>>>>>> origin/summer22
             for tty in ttys: tty.makeTTYVariations()
         data_entries = sum(tty.getEntries() for tty in self._allData['data'])
         for p in self.listProcesses():
