1738d6265aff3bc552993999a519ccc631ab4c91
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-12-07 02:07:03 +0100

    new code for linearity

---
+++ plotter/linearity.py
@@ -0,0 +1,152 @@
+import os, math, optparse, ROOT
+ROOT.gROOT.SetBatch(1)
+from simple_plot import getCanvas, doLegend
+
+def fitOne(filein,selection,erange,suffix):
+
+    xmin,xmax=erange
+    nbins = 50
+    
+    work = ROOT.RooWorkspace()
+    work.factory('CBShape::cb(x[{xmin},{xmax}],mean[0,{mmax}],sigma[1,0.8,10],alpha[1,0.1,10],n[5])'.format(xmin=xmin,xmax=xmax,mmax=min(50,xmax)))
+    work.factory("a[0,-100,1000]")
+    work.factory("b[0,-100,100]")
+    work.factory("c[0,-100,100]")
+    work.factory("d[0,-100,100]")
+    work.factory('Chebychev::bkg(x,{a,b,c,d})')
+    
+    x = work.var('x')
+
+    var = 'sc_integral*6/8000'
+
+    mark = ROOT.kFullCircle
+    line = ROOT.kAzure-6
+    
+    rf = ROOT.TFile.Open(filein)
+    t = rf.Get('Events')
+    spectrum = ROOT.TH1F('spectrum','',nbins,xmin,xmax)
+    spectrum.SetMarkerStyle(mark)
+    spectrum.SetLineColor(ROOT.kBlack)
+    t.Draw('{var}>>spectrum'.format(var=var),'({var}>{xmin}) * ({var}<{xmax}) * ({sel})'.format(var=var,xmin=xmin,xmax=xmax,sel=selection))
+
+    work.factory('nSig[{ns}]'.format(ns=0.5*spectrum.Integral()))
+    work.factory('nBkg[{ns}]'.format(ns=0.5*spectrum.Integral()))
+    work.factory("SUM::pdfTot(nSig*cb,nBkg*bkg)");
+
+    work.Print()
+    
+    rooData = ROOT.RooDataHist("spectrum","spectrum",ROOT.RooArgList(work.var("x")),spectrum)
+    getattr(work,'import')(rooData)
+
+    frame = x.frame()
+    frame.SetTitle('')
+    
+    # fit spectrum
+    rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark),ROOT.RooFit.MarkerSize(2))
+    pdf = work.pdf('pdfTot')
+    pdf.fitTo(rooData,ROOT.RooFit.Save(),ROOT.RooFit.PrintLevel(10))
+    pdf.plotOn(frame,ROOT.RooFit.LineColor(line))
+    pdf.plotOn(frame,ROOT.RooFit.Components("bkg"),ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kCyan))
+    pdf.plotOn(frame,ROOT.RooFit.Components("cb"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kRed))
+    rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark))
+
+    frame.GetYaxis().SetTitle("superclusters")
+    frame.GetXaxis().SetTitle("Energy (keV)")
+    frame.GetXaxis().SetTitleOffset(1.2)
+
+    c = getCanvas()
+    frame.Draw()
+
+    for ext in ['pdf','png','root']:
+        c.SaveAs('energy_{suff}.{ext}'.format(ext=ext,suff=suffix))
+
+    m    = work.var('mean').getVal()
+    merr = work.var('mean').getError()
+    s    = work.var('sigma').getVal()
+    serr = work.var('sigma').getError()
+
+    return [m,merr,s,serr]
+
+if __name__ == "__main__":
+
+    parser = optparse.OptionParser(usage='usage: %prog [opts] ', version='%prog 1.0')
+    (options, args) = parser.parse_args()
+
+    base_sel = "(TMath::Hypot(sc_xmean-2304/2,sc_ymean-2304/2)<800)"
+
+    energies = {"Fe" : 6,
+                "Cu" : 8,
+                "Rb" : 13,
+                "Mo" : 18,
+                "Ag" : 23.5,
+                "Ba" : 34.4
+                }
+        
+    
+    extras = {"Cu" : "(0.152*sc_length < 80)  * (sc_rms>7.5) * (run>=5801 && run<=5805)",
+              "Rb" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5806 && run<=5810)",
+              "Mo" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5811 && run<=5820)",
+              "Ag" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5821 && run<=5830)",
+              "Ba" : "(0.152*sc_length < 120) * (sc_rms>6.5) * (run>=5831 && run<=5845)"}
+
+    eranges = {"Cu" : (1,16),
+               "Rb" : (1,30),
+               "Mo" : (7,32),
+               "Ag" : (9,40),
+               "Ba" : (10,60)}
+    respdic = {}
+    respdic["Fe"] = [energies['Fe'],0,0.9,0]
+    
+    for mat,extracut in extras.items():
+        respdic[mat] = fitOne("/Users/emanuele/data/cygnus/RECO/lnf/reco_multisource.root ",'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mat)
+
+
+    fout = ROOT.TFile.Open("linearity.root","recreate")
+    resp = ROOT.TGraphErrors(len(energies))
+    reso = ROOT.TGraphErrors(len(energies))
+    i=0
+    for mat,etrue in energies.items():
+        resp.SetPoint(i,energies[mat],respdic[mat][0])
+        resp.SetPointError(i,0,respdic[mat][1])
+        reso.SetPoint(i,energies[mat],100*respdic[mat][2]/respdic[mat][0])
+        reso.SetPointError(i,0,100*respdic[mat][3]/respdic[mat][0])
+        i+=1
+
+    
+    ## response linearity
+    c = getCanvas()
+    resp.SetTitle('')
+    resp.Draw("AP")
+    resp.SetMarkerStyle(ROOT.kFullCircle)
+    resp.SetMarkerSize(2)
+    resp.GetXaxis().SetLimits(0,50)
+    resp.GetXaxis().SetTitle("Expected energy (keV)")
+    resp.GetYaxis().SetLimits(0,8e4)
+    resp.GetYaxis().SetRangeUser(0,50)
+    resp.GetYaxis().SetTitle("Measured energy (keV)")
+
+    resp.Fit("pol1")
+
+    line = ROOT.TLine(0,0,50,50)
+    line.SetLineColor(ROOT.kBlue)
+    line.SetLineStyle(ROOT.kDashed)
+    line.Draw()
+    
+    fout.cd()
+    resp.Write()
+    c.Write()
+    c.SaveAs("linearity.pdf")
+    fout.Close()
+    
+    # energy resolution
+    c = getCanvas()
+    reso.SetTitle('')
+    reso.Draw("AP")
+    reso.SetMarkerStyle(ROOT.kFullCircle)
+    reso.SetMarkerSize(2)
+    reso.GetXaxis().SetLimits(0,50)
+    reso.GetXaxis().SetTitle("Energy (keV)")
+    reso.GetYaxis().SetLimits(0,30)
+    reso.GetYaxis().SetRangeUser(0,30)
+    reso.GetYaxis().SetTitle("#sigma_{E}/E (%)")        
+    c.SaveAs("resolution.pdf")
