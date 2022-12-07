490321658691a81ad1b98c0abe799626a72f6ad4
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-12-07 23:56:33 +0100

    add Cruijff PDF and improve linearity with 2 peaks joined fits

--- plotter/linearity.py
+++ plotter/linearity.py
@@ -4,8 +4,9 @@ from simple_plot import getCanvas, doLegend
 from mcPlots import doTinyCmsPrelim
 
 ROOT.gROOT.LoadMacro('./fitter/libCpp/RooDoubleCBFast.cc+')
+ROOT.gROOT.LoadMacro('./fitter/libCpp/RooCruijff.cc+')
 
-def fitOne(filein,selection,erange,suffix,energies,nbins):
+def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
 
     var = 'sc_integral*6/8000'
 
@@ -23,18 +24,20 @@ def fitOne(filein,selection,erange,suffix,energies,nbins):
     av=energies[0]
 
     work = ROOT.RooWorkspace()
-    work.factory("mean1[{av},{mmin},{mmax}]".format(av=av,mmin=0.7*av,mmax=1.3*av))
-    #work.factory('Gaussian::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,1])'.format(xmin=xmin,xmax=xmax))
-    if suffix in ['Cu','Rb','Ag']:
+    work.factory("mean1[{av},{mmin},{mmax}]".format(av=av,mmin=mrange[0],mmax=mrange[1]))
+    if suffix in ['XXX']:
         work.factory('CBShape::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,3],alpha[1,0.1,10],n[9,1,20])'.format(xmin=xmin,xmax=xmax))
+    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba']:
+        work.factory('Cruijff::cb1(x[{xmin},{xmax}],mean1,sigmaL[1,0.5,4],sigma[1,0.2,4],alphaL[1,0.1,10],alphaR[1,0.1,10])'.format(xmin=xmin,xmax=xmax))
     else:
-        work.factory('DoubleCBFast::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,1],alpha1[1,0.1,10],n1[5,1,10],alpha2[1,0.05,10],n2[5,1,10])'.format(xmin=xmin,xmax=xmax))
+        work.factory('DoubleCBFast::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.8,10],alpha1[1,0.01,10],n1[5,1,20],alpha2[1,0.01,10],n2[5,1,20])'.format(xmin=xmin,xmax=xmax))
     work.factory("expr::mean2('mean1+delta',mean1,delta[{delta}])".format(delta=deltae))
-    #work.factory('Gaussian::cb2(x[{xmin},{xmax}],mean2,sigma[1,0.5,1])'.format(xmin=xmin,xmax=xmax))
-    if suffix in ['Cu','Rb','Ag']:
+    if suffix in ['XXX']:
         work.factory('CBShape::cb2(x[{xmin},{xmax}],mean2,sigma,alpha,n)'.format(xmin=xmin,xmax=xmax))
+    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba']:
+        work.factory('Cruijff::cb2(x[{xmin},{xmax}],mean2,sigmaL,sigma,alphaL,alphaR)'.format(xmin=xmin,xmax=xmax))
     else:
-        work.factory('DoubleCBFast::cb2(x[{xmin},{xmax}],mean2,sigma[1,0.5,1],alpha1,n1,alpha2,n2)'.format(xmin=xmin,xmax=xmax))
+        work.factory('DoubleCBFast::cb2(x[{xmin},{xmax}],mean2,sigma,alpha1,n1,alpha2,n2)'.format(xmin=xmin,xmax=xmax))
 
     work.factory("a[0,-100,1000]")
     work.factory("b[0,-100,100]")
@@ -42,16 +45,26 @@ def fitOne(filein,selection,erange,suffix,energies,nbins):
     work.factory("d[0,-100,100]")
     work.factory("e[0,-100,100]")
     #work.factory('Chebychev::bkg(x,{a,b,c,d,e})')
-    work.factory('Bernstein::bkg(x,{a,b,c,d})')
+    if suffix in ['Fe']:
+        work.factory('Bernstein::bkg(x,{a})')
+    else:
+        work.factory('Bernstein::bkg(x,{a,b,c,d})')
     
     x = work.var('x')
 
-    print ("ssss = ",spectrum.Integral())
     work.factory('nSig1[{ns}]'.format(ns=0.2*spectrum.Integral()))
-    work.factory("expr::nSig2('nSig1*frac',nSig1,frac[0.2,0,0.2])")
+    work.factory("expr::nSig2('nSig1*frac',nSig1,frac[0.1,0,0.3])")
     work.factory('nBkg[{ns}]'.format(ns=0.5*spectrum.Integral()))
     work.factory("SUM::pdfTot(nSig1*cb1,nSig2*cb2,nBkg*bkg)");
 
+    if suffix=='Fe':
+        work.var('nBkg').setVal(0)
+        work.var('nBkg').setConstant(ROOT.kTRUE)
+    if suffix=='Ba':
+        work.var('sigma').setRange(1,6)
+    elif suffix=='Cu':
+        work.var('sigma').setRange(0.5,1)
+        
     work.Print()
     
     rooData = ROOT.RooDataHist("spectrum","spectrum",ROOT.RooArgList(work.var("x")),spectrum)
@@ -89,6 +102,8 @@ def fitOne(filein,selection,erange,suffix,energies,nbins):
     leg.AddEntry("pbkg","Background Model","L")
     leg.Draw("Same")
 
+    doTinyCmsPrelim("CYGNO","(LIME)",lumi=0,textSize=0.04,xoffs=-0.05)
+    
     for ext in ['pdf','png','root']:
         c.SaveAs('energy_{suff}.{ext}'.format(ext=ext,suff=suffix))
 
@@ -106,52 +121,73 @@ if __name__ == "__main__":
 
     base_sel = "(TMath::Hypot(sc_xmean-2304/2,sc_ymean-2304/2)<800)"
 
-    energies = {"Fe" : (5.9,6),
-                "Cu" : (8.04,  8.91),
-                "Rb" : (13.37, 14.97),
-                "Mo" : (17.44, 19.63),
-                "Ag" : (22.10, 24.99),
-                "Ba" : (32.06, 36.55)
+    materials = ["Rb","Cu","Mo","Ag","Ba","Fe"]
+
+    energies = {"Fe" : [5.9,   6.5],
+                "Cu" : [8.04,  8.91],
+                "Rb" : [13.37, 14.97],
+                "Mo" : [17.44, 19.63],
+                "Ag" : [22.10, 24.99],
+                "Ba" : [32.06, 36.55],
                 }
         
     
-    extras = {"Cu" : "(0.152*sc_length < 80)  * (sc_rms>7.5) * (run>=5801 && run<=5805)",
+    extras = {"Fe" : "(0.152*sc_length < 80)  * (sc_rms>7.5)",
+              "Cu" : "(0.152*sc_length < 80)  * (sc_rms>7.5) * (run>=5801 && run<=5805)",
               "Rb" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5806 && run<=5810)",
               "Mo" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5811 && run<=5820)",
               "Ag" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5821 && run<=5830)",
               "Ba" : "(0.152*sc_length < 120) * (sc_rms>6.5) * (run>=5831 && run<=5845)"}
 
 
-    eranges = {"Cu" : (2,16),
+    eranges = {"Fe" : (2,12),
+               "Cu" : (2,16),
                "Rb" : (1,25),
-               "Mo" : (8,32),
+               "Mo" : (8,30),
                "Ag" : (9,40),
                "Ba" : (10,60)}
 
-    nbins = {"Cu" : 45,
+    nbins = {"Fe" : 120,
+             "Cu" : 45,
              "Rb" : 45,
-             "Mo" : 60,
-             "Ag" : 60,
-             "Ba" : 60}
-        
+             "Mo" : 55,
+             "Ag" : 55,
+             "Ba" : 50}
+
+    mrange = {"Fe" : (5,8),
+              "Cu" : (6,9),
+              "Rb" : (9,11.5),
+              "Mo" : (12,18),
+              "Ag" : (17,22),
+              "Ba" : (25,36)}
+
+    rfiles = {"Fe" : "reco_fe_26cm.root",
+              "Cu" : "reco_multisource.root",
+              "Rb" : "reco_multisource.root",
+              "Mo" : "reco_multisource.root",
+              "Ag" : "reco_multisource.root",
+              "Ba" : "reco_multisource.root",
+              }
+              
     respdic = {}
-    respdic["Fe"] = [energies['Fe'],0,0.9,0]
+    respdic["Fe"] = [energies['Fe'][0],0,0.9,0]
 
-    mat = 'Ba'
-    fitOne("trees-lnf/reco_multisource.root ",'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mat,energies[mat],nbins[mat])
-"""    
-    for mat,extracut in extras.items():
-        respdic[mat] = fitOne("/Users/emanuele/data/cygnus/RECO/lnf/reco_multisource.root ",'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mat)
+    #    mat = 'Cu'
+    #    respdic[mat] = fitOne("trees-lnf/%s" % rfiles[mat],'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mrange[mat],mat,energies[mat],nbins[mat])
 
+    for mat in materials:
+        respdic[mat] = fitOne("trees-lnf/%s" % rfiles[mat],'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mrange[mat],mat,energies[mat],nbins[mat])
 
+    calibFe = energies['Fe'][0]/respdic['Fe'][0]
+        
     fout = ROOT.TFile.Open("linearity.root","recreate")
     resp = ROOT.TGraphErrors(len(energies))
     reso = ROOT.TGraphErrors(len(energies))
     i=0
     for mat,etrue in energies.items():
-        resp.SetPoint(i,energies[mat],respdic[mat][0])
-        resp.SetPointError(i,0,respdic[mat][1])
-        reso.SetPoint(i,energies[mat],100*respdic[mat][2]/respdic[mat][0])
+        resp.SetPoint(i,energies[mat][0],respdic[mat][0]*calibFe)
+        resp.SetPointError(i,0,respdic[mat][1]*calibFe)
+        reso.SetPoint(i,energies[mat][0],100*respdic[mat][2]/respdic[mat][0])
         reso.SetPointError(i,0,100*respdic[mat][3]/respdic[mat][0])
         i+=1
 
@@ -168,13 +204,15 @@ if __name__ == "__main__":
     resp.GetYaxis().SetRangeUser(0,50)
     resp.GetYaxis().SetTitle("Measured energy (keV)")
 
-    resp.Fit("pol1")
+    #resp.Fit("pol1")
 
     line = ROOT.TLine(0,0,50,50)
     line.SetLineColor(ROOT.kBlue)
     line.SetLineStyle(ROOT.kDashed)
     line.Draw()
-    
+
+    doTinyCmsPrelim("CYGNO","(LIME)",lumi=0,textSize=0.04,xoffs=-0.05)
+
     fout.cd()
     resp.Write()
     c.Write()
@@ -193,6 +231,10 @@ if __name__ == "__main__":
     reso.GetYaxis().SetLimits(0,30)
     reso.GetYaxis().SetRangeUser(0,30)
     reso.GetYaxis().SetTitle("#sigma_{E}/E (%)")        
+    doTinyCmsPrelim("CYGNO","(LIME)",lumi=0,textSize=0.04,xoffs=-0.05)
     for ext in ['pdf','png','root']:
         c.SaveAs('resolution.{ext}'.format(ext=ext))
-"""
+
+
+
+
