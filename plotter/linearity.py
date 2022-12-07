9624578d2c7524b87ff3521a8e4ebc7feeccba72
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-12-07 17:12:12 +0100

    first round of fit with double peak

--- plotter/linearity.py
+++ plotter/linearity.py
@@ -1,37 +1,56 @@
 import os, math, optparse, ROOT
 ROOT.gROOT.SetBatch(1)
 from simple_plot import getCanvas, doLegend
+from mcPlots import doTinyCmsPrelim
 
-def fitOne(filein,selection,erange,suffix):
+ROOT.gROOT.LoadMacro('./fitter/libCpp/RooDoubleCBFast.cc+')
 
-    xmin,xmax=erange
-    nbins = 50
-    
-    work = ROOT.RooWorkspace()
-    work.factory('CBShape::cb(x[{xmin},{xmax}],mean[0,{mmax}],sigma[1,0.8,10],alpha[1,0.1,10],n[5])'.format(xmin=xmin,xmax=xmax,mmax=min(50,xmax)))
-    work.factory("a[0,-100,1000]")
-    work.factory("b[0,-100,100]")
-    work.factory("c[0,-100,100]")
-    work.factory("d[0,-100,100]")
-    work.factory('Chebychev::bkg(x,{a,b,c,d})')
-    
-    x = work.var('x')
+def fitOne(filein,selection,erange,suffix,energies,nbins):
 
     var = 'sc_integral*6/8000'
 
-    mark = ROOT.kFullCircle
+    xmin,xmax=erange
+    mark = ROOT.kFullSquare
     line = ROOT.kAzure-6
-    
+
     rf = ROOT.TFile.Open(filein)
     t = rf.Get('Events')
     spectrum = ROOT.TH1F('spectrum','',nbins,xmin,xmax)
     spectrum.SetMarkerStyle(mark)
     spectrum.SetLineColor(ROOT.kBlack)
     t.Draw('{var}>>spectrum'.format(var=var),'({var}>{xmin}) * ({var}<{xmax}) * ({sel})'.format(var=var,xmin=xmin,xmax=xmax,sel=selection))
+    deltae = energies[1]-energies[0]
+    av=energies[0]
 
-    work.factory('nSig[{ns}]'.format(ns=0.5*spectrum.Integral()))
+    work = ROOT.RooWorkspace()
+    work.factory("mean1[{av},{mmin},{mmax}]".format(av=av,mmin=0.7*av,mmax=1.3*av))
+    #work.factory('Gaussian::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,1])'.format(xmin=xmin,xmax=xmax))
+    if suffix in ['Cu','Rb']:
+        work.factory('CBShape::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,1.5],alpha[1,0.1,10],n[9,1,20])'.format(xmin=xmin,xmax=xmax))
+    else:
+        work.factory('DoubleCBFast::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,1],alpha1[1,0.1,10],n1[5,1,10],alpha2[1,0.1,10],n2[5,1,10])'.format(xmin=xmin,xmax=xmax))
+    work.factory("expr::mean2('mean1+delta',mean1,delta[{delta}])".format(delta=deltae))
+    #work.factory('Gaussian::cb2(x[{xmin},{xmax}],mean2,sigma[1,0.5,1])'.format(xmin=xmin,xmax=xmax))
+    if suffix in ['Cu','Rb']:
+        work.factory('CBShape::cb2(x[{xmin},{xmax}],mean2,sigma,alpha,n)'.format(xmin=xmin,xmax=xmax))
+    else:
+        work.factory('DoubleCBFast::cb2(x[{xmin},{xmax}],mean2,sigma[1,0.5,1],alpha1,n1,alpha2,n2)'.format(xmin=xmin,xmax=xmax))
+
+    work.factory("a[0,-100,1000]")
+    work.factory("b[0,-100,100]")
+    work.factory("c[0,-100,100]")
+    work.factory("d[0,-100,100]")
+    work.factory("e[0,-100,100]")
+    #work.factory('Chebychev::bkg(x,{a,b,c,d,e})')
+    work.factory('Bernstein::bkg(x,{a,b,c,d})')
+    
+    x = work.var('x')
+
+    print ("ssss = ",spectrum.Integral())
+    work.factory('nSig1[{ns}]'.format(ns=0.2*spectrum.Integral()))
+    work.factory("expr::nSig2('nSig1*frac',nSig1,frac[0.2,0,0.5])")
     work.factory('nBkg[{ns}]'.format(ns=0.5*spectrum.Integral()))
-    work.factory("SUM::pdfTot(nSig*cb,nBkg*bkg)");
+    work.factory("SUM::pdfTot(nSig1*cb1,nSig2*cb2,nBkg*bkg)");
 
     work.Print()
     
@@ -42,12 +61,13 @@ def fitOne(filein,selection,erange,suffix):
     frame.SetTitle('')
     
     # fit spectrum
-    rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark),ROOT.RooFit.MarkerSize(2))
+    rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark),ROOT.RooFit.MarkerSize(2),ROOT.RooFit.Name('data'))
     pdf = work.pdf('pdfTot')
     pdf.fitTo(rooData,ROOT.RooFit.Save(),ROOT.RooFit.PrintLevel(10))
-    pdf.plotOn(frame,ROOT.RooFit.LineColor(line))
-    pdf.plotOn(frame,ROOT.RooFit.Components("bkg"),ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kCyan))
-    pdf.plotOn(frame,ROOT.RooFit.Components("cb"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kRed))
+    pdf.plotOn(frame,ROOT.RooFit.LineColor(line), ROOT.RooFit.Name('ptot'))
+    pdf.plotOn(frame,ROOT.RooFit.Components("bkg"),ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kCyan), ROOT.RooFit.Name('pbkg'))
+    pdf.plotOn(frame,ROOT.RooFit.Components("cb1"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kRed), ROOT.RooFit.Name('pcb1'))
+    pdf.plotOn(frame,ROOT.RooFit.Components("cb2"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kOrange), ROOT.RooFit.Name('pcb2'))
     rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark))
 
     frame.GetYaxis().SetTitle("superclusters")
@@ -57,11 +77,23 @@ def fitOne(filein,selection,erange,suffix):
     c = getCanvas()
     frame.Draw()
 
+    # Add legend
+    leg = ROOT.TLegend(0.6,0.6,0.85,0.88)
+    leg.SetFillStyle(0)
+    leg.SetLineColor(0)
+    leg.SetTextSize(0.02)
+    leg.AddEntry("pdata","data with {source}".format(source=suffix),"ep")
+    leg.AddEntry("tot","Parametric Total Model","L")
+    leg.AddEntry("pcb1","Signal Model 1","L")
+    leg.AddEntry("pcb2","Signal Model 2","L")
+    leg.AddEntry("pbkg","Background Model","L")
+    leg.Draw("Same")
+
     for ext in ['pdf','png','root']:
         c.SaveAs('energy_{suff}.{ext}'.format(ext=ext,suff=suffix))
 
-    m    = work.var('mean').getVal()
-    merr = work.var('mean').getError()
+    m    = work.var('mean1').getVal()
+    merr = work.var('mean1').getError()
     s    = work.var('sigma').getVal()
     serr = work.var('sigma').getError()
 
@@ -74,12 +106,12 @@ if __name__ == "__main__":
 
     base_sel = "(TMath::Hypot(sc_xmean-2304/2,sc_ymean-2304/2)<800)"
 
-    energies = {"Fe" : 6,
-                "Cu" : 8,
-                "Rb" : 13,
-                "Mo" : 17,
-                "Ag" : 22,
-                "Ba" : 32
+    energies = {"Fe" : (5.9,6),
+                "Cu" : (8.04,  8.91),
+                "Rb" : (13.37, 14.97),
+                "Mo" : (17.44, 19.63),
+                "Ag" : (22.10, 24.99),
+                "Ba" : (32.06, 36.55)
                 }
         
     
@@ -89,14 +121,25 @@ if __name__ == "__main__":
               "Ag" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5821 && run<=5830)",
               "Ba" : "(0.152*sc_length < 120) * (sc_rms>6.5) * (run>=5831 && run<=5845)"}
 
-    eranges = {"Cu" : (1,16),
-               "Rb" : (1,30),
-               "Mo" : (7,32),
+
+    eranges = {"Cu" : (2,16),
+               "Rb" : (1,25),
+               "Mo" : (8,32),
                "Ag" : (9,40),
                "Ba" : (10,60)}
+
+    nbins = {"Cu" : 45,
+             "Rb" : 45,
+             "Mo" : 75,
+             "Ag" : 100,
+             "Ba" : 100}
+        
     respdic = {}
     respdic["Fe"] = [energies['Fe'],0,0.9,0]
-    
+
+    mat = 'Cu'
+    fitOne("trees-lnf/reco_multisource.root ",'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mat,energies[mat],nbins[mat])
+"""    
     for mat,extracut in extras.items():
         respdic[mat] = fitOne("/Users/emanuele/data/cygnus/RECO/lnf/reco_multisource.root ",'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mat)
 
@@ -152,3 +195,4 @@ if __name__ == "__main__":
     reso.GetYaxis().SetTitle("#sigma_{E}/E (%)")        
     for ext in ['pdf','png','root']:
         c.SaveAs('resolution.{ext}'.format(ext=ext))
+"""
