6031b04e2c73e062adad6e9480a7cbe59b6bc165
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2022-12-14 09:29:38 +0100

    add titanium fit

--- plotter/linearity.py
+++ plotter/linearity.py
@@ -27,17 +27,23 @@ def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
     work.factory("mean1[{av},{mmin},{mmax}]".format(av=av,mmin=mrange[0],mmax=mrange[1]))
     if suffix in ['XXX']:
         work.factory('CBShape::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.5,3],alpha[1,0.1,10],n[9,1,20])'.format(xmin=xmin,xmax=xmax))
-    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba']:
+    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba','Ti']:
         work.factory('Cruijff::cb1(x[{xmin},{xmax}],mean1,sigmaL[1,0.5,4],sigma[1,0.2,4],alphaL[1,0.1,10],alphaR[0,0.1,10])'.format(xmin=xmin,xmax=xmax))
     else:
         work.factory('DoubleCBFast::cb1(x[{xmin},{xmax}],mean1,sigma[1,0.8,10],alpha1[1,0.01,10],n1[5,1,20],alpha2[1,0.01,10],n2[5,1,20])'.format(xmin=xmin,xmax=xmax))
     work.factory("expr::mean2('mean1+delta',mean1,delta[{delta}])".format(delta=deltae))
     if suffix in ['XXX']:
         work.factory('CBShape::cb2(x[{xmin},{xmax}],mean2,sigma,alpha,n)'.format(xmin=xmin,xmax=xmax))
-    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba']:
+    elif suffix in ['Cu','Fe','Rb','Mo','Ag','Ba','Ti']:
         work.factory('Cruijff::cb2(x[{xmin},{xmax}],mean2,sigmaL,sigma,alphaL,alphaR)'.format(xmin=xmin,xmax=xmax))
     else:
         work.factory('DoubleCBFast::cb2(x[{xmin},{xmax}],mean2,sigma,alpha1,n1,alpha2,n2)'.format(xmin=xmin,xmax=xmax))
+    if suffix=='Ti':
+        # this is to model the Fe55 peak
+        work.factory('Cruijff::cb3(x[{xmin},{xmax}],mean3[5.9,5.5,7],sigmaL,sigma,alphaL,alphaR)'.format(xmin=xmin,xmax=xmax))
+        work.factory("expr::mean4('mean3+deltaFe',mean3,deltaFe[0.6])")
+        work.factory('Cruijff::cb4(x[{xmin},{xmax}],mean4,sigmaL,sigma,alphaL,alphaR)'.format(xmin=xmin,xmax=xmax))
+
 
     work.factory("a[0,-100,1000]")
     work.factory("b[0,-100,100]")
@@ -54,7 +60,12 @@ def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
     work.factory('nSig1[{ns}]'.format(ns=0.2*spectrum.Integral()))
     work.factory("expr::nSig2('nSig1*frac',nSig1,frac[0.1,0,0.3])")
     work.factory('nBkg[{ns}]'.format(ns=0.5*spectrum.Integral()))
-    work.factory("SUM::pdfTot(nSig1*cb1,nSig2*cb2,nBkg*bkg)");
+    if suffix!='Ti':
+        work.factory("SUM::pdfTot(nSig1*cb1,nSig2*cb2,nBkg*bkg)");
+    else:
+        work.factory('nSig3[{ns}]'.format(ns=0.7*spectrum.Integral()))
+        work.factory("expr::nSig4('nSig3*frac2',nSig3,frac2[0.3,0.1,0.4])")
+        work.factory("SUM::pdfTot(nSig1*cb1,nSig2*cb2,nSig3*cb3,nSig4*cb4,nBkg*bkg)");
 
     if suffix=='Fe':
         work.var('nBkg').setVal(0)
@@ -63,13 +74,28 @@ def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
         work.var('sigma').setRange(1,6)
     elif suffix=='Cu':
         work.var('sigma').setRange(0.5,1)
-    if suffix in ['Ag','Ba']:
-        work.var('alphaR').setConstant(ROOT.kTRUE)
+    if suffix in ['Ba']:
+        work.var('alphaR').setRange(0.,0.3)
     if suffix in ['Rb']:
          work.var('alphaR').setVal(1)
     elif suffix in ['Cu']:
         work.var('alphaR').setRange(0.1,10)
         work.var('alphaR').setConstant(ROOT.kFALSE)
+    if suffix=='Ti':
+        work.var('frac2').setRange(0.2,0.5); work.var('frac').setRange(0.2,0.5); 
+        work.var('alphaL').setVal(0.251);    #work.var('alphaL').setConstant(ROOT.kTRUE)
+        work.var('alphaR').setVal(0.252);    #work.var('alphaR').setConstant(ROOT.kTRUE)
+        work.var('sigma').setVal(0.616);     #work.var('sigma').setConstant(ROOT.kTRUE)
+        work.var('sigmaL').setVal(0.616);    #work.var('sigmaL').setConstant(ROOT.kTRUE)
+        work.var('mean3').setVal(6.33);      #work.var('mean3').setConstant(ROOT.kTRUE)
+        work.var('mean1').setVal(4.5)
+        
+        work.var('a').setVal(2.62289e-02);  work.var('a').setConstant(ROOT.kTRUE)
+        work.var('b').setVal(8.81561e-02);  work.var('b').setConstant(ROOT.kTRUE)
+        work.var('c').setVal(6.68959e-04);  work.var('c').setConstant(ROOT.kTRUE)
+        work.var('d').setVal(2.18863e-02);  work.var('d').setConstant(ROOT.kTRUE)
+        work.var('e').setVal(2.00491e-02);  work.var('e').setConstant(ROOT.kTRUE)
+
         
     work.Print()
     
@@ -87,6 +113,9 @@ def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
     pdf.plotOn(frame,ROOT.RooFit.Components("bkg"),ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kCyan), ROOT.RooFit.Name('pbkg'))
     pdf.plotOn(frame,ROOT.RooFit.Components("cb1"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kRed), ROOT.RooFit.Name('pcb1'))
     pdf.plotOn(frame,ROOT.RooFit.Components("cb2"), ROOT.RooFit.LineStyle(ROOT.kDotted), ROOT.RooFit.LineColor(ROOT.kOrange), ROOT.RooFit.Name('pcb2'))
+    if suffix=='Ti':
+        pdf.plotOn(frame,ROOT.RooFit.Components("cb3"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kSpring+4), ROOT.RooFit.Name('pcb3'))
+        pdf.plotOn(frame,ROOT.RooFit.Components("cb4"), ROOT.RooFit.LineStyle(ROOT.kDashed), ROOT.RooFit.LineColor(ROOT.kSpring+1), ROOT.RooFit.Name('pcb4'))        
     rooData.plotOn(frame,ROOT.RooFit.MarkerStyle(mark))
 
     frame.GetYaxis().SetTitle("superclusters")
@@ -119,6 +148,9 @@ def fitOne(filein,selection,erange,mrange,suffix,energies,nbins):
     leg.AddEntry("tot","Parametric Total Model","L")
     leg.AddEntry("pcb1","Signal Model 1","L")
     leg.AddEntry("pcb2","Signal Model 2","L")
+    if suffix=='Ti':
+        leg.AddEntry("pcb3","^{55}Fe Signal Model 1","L")
+        leg.AddEntry("pcb4","^{55}Fe Signal Model 2","L")        
     leg.AddEntry("pbkg","Background Model","L")
     leg.Draw("Same")
 
@@ -141,7 +173,8 @@ if __name__ == "__main__":
 
     base_sel = "(TMath::Hypot(sc_xmean-2304/2,sc_ymean-2304/2)<800)"
 
-    materials = ["Rb","Cu","Mo","Ag","Ba","Fe"]
+    materials = ["Cu","Rb","Mo","Ag","Ba","Fe","Ti"]
+#    materials = ["Ti"]
 
     energies = {"Fe" : [5.9,   6.5],
                 "Cu" : [8.04,  8.91],
@@ -149,6 +182,7 @@ if __name__ == "__main__":
                 "Mo" : [17.44, 19.63],
                 "Ag" : [22.10, 24.99],
                 "Ba" : [32.06, 36.55],
+                "Ti" : [4.5, 4.9],
                 }
         
     
@@ -157,7 +191,8 @@ if __name__ == "__main__":
               "Rb" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5806 && run<=5810)",
               "Mo" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5811 && run<=5820)",
               "Ag" : "(0.152*sc_length < 80)  * (sc_rms>6.5) * (run>=5821 && run<=5830)",
-              "Ba" : "(0.152*sc_length < 120) * (sc_rms>6.5) * (run>=5831 && run<=5845)"}
+              "Ba" : "(0.152*sc_length < 120) * (sc_rms>6.5) * (run>=5831 && run<=5845)",
+              "Ti" : "(0.152*sc_length < 80)  * (sc_rms>7.5) * (run>=6122 && run<=6131)",}
 
 
     eranges = {"Fe" : (2,12),
@@ -165,21 +200,24 @@ if __name__ == "__main__":
                "Rb" : (1,25),
                "Mo" : (8,30),
                "Ag" : (9,40),
-               "Ba" : (11,60)}
+               "Ba" : (11,60),
+               "Ti" : (1,11)}
 
     nbins = {"Fe" : 120,
              "Cu" : 45,
              "Rb" : 45,
              "Mo" : 55,
              "Ag" : 55,
-             "Ba" : 50}
+             "Ba" : 50,
+             "Ti" : 50,}
 
     mrange = {"Fe" : (5,8),
               "Cu" : (6,9),
               "Rb" : (9,11.5),
               "Mo" : (12,18),
               "Ag" : (17,22),
-              "Ba" : (25,36)}
+              "Ba" : (25,36),
+              "Ti" : (3.8,5.3),}
 
     rfiles = {"Fe" : "reco_fe_26cm.root",
               "Cu" : "reco_multisource.root",
@@ -187,14 +225,12 @@ if __name__ == "__main__":
               "Mo" : "reco_multisource.root",
               "Ag" : "reco_multisource.root",
               "Ba" : "reco_multisource.root",
+              "Ti" : "reco_titanium.root",
               }
               
     respdic = {}
     respdic["Fe"] = [energies['Fe'][0],0,0.9,0]
 
-    #mat = 'Cu'
-    #respdic[mat] = fitOne("trees-lnf/%s" % rfiles[mat],'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mrange[mat],mat,energies[mat],nbins[mat])
-    
     for mat in materials:
         respdic[mat] = fitOne("trees-lnf/%s" % rfiles[mat],'({base})*({extra})'.format(base=base_sel,extra=extras[mat]),eranges[mat],mrange[mat],mat,energies[mat],nbins[mat])
 
