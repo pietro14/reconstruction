db14e21ff20b9e872a540584f8b63c5dbf685608
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2023-08-02 21:40:37 +0200

    update to the new format of the runlog to parse the pedestal runs

--- utilities.py
+++ utilities.py
@@ -291,8 +291,9 @@ class utils:
                 next(csvreader)
                 for row in reversed(list(csvreader)):
                     runkey,runtype,comment = row[:3]
+                    pedestal_flag = int(row[20])
                     nevents = int(row[-2]) if str(row[-2]).strip()!="NULL" else 0
-                    if int(runkey)<=int(options.run) and (":PED:" in runtype) and nevents>=100:
+                    if int(runkey)<=int(options.run) and pedestal_flag and nevents>=100:
                         options.pedrun = int(runkey)
                         print("Will use pedestal run %05d which has comment: '%s' and n of events: '%d'" % (int(runkey),comment,int(nevents)))
                         break
