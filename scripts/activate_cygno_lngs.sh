7b05f793c2d7e001fd48e0f941938a650e62e9d4
giorgio <GioDho>
2024-01-25 19:09:58 +0100

    For LNGS cluster, use as default Igor environment

--- scripts/activate_cygno_lngs.sh
+++ scripts/activate_cygno_lngs.sh
@@ -1,8 +1,9 @@
 echo "Entering the CYGNO virtual environment software..."
 echo "Setting up python3.8 from central installation on /nfs..."
-export PYTHONPATH=$PYTHONPATH:/nfs/cygno/users/$USER/local/lib/python3.8/site-packages/
-alias python="python3.8"
-alias pip="pip3.8"
+source /nfs/cygno/users/abrittac/reco_env/bin/activate
+#export PYTHONPATH=$PYTHONPATH:/nfs/cygno/users/$USER/local/lib/python3.8/site-packages/
+#alias python="python3.8"
+#alias pip="pip3.8"
 echo "Setting up ROOT..."
 source /nfs/cygno/software/root-v6-22-02-py38-install/bin/thisroot.sh
 echo "DONE."
